36c8b39aa3b486aaff5ea99dd972c2be78032586
alexandrovbg <cecko.aleksandrov@gmail.com>
2021-06-02 19:18:10 +0300

    Create Search Component

---
+++ client/src/components/Search/Search.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import './Search.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Button, Form, FormControl } from 'react-bootstrap';
+import MoviesList from './MoviesList/MoviesList';
+
+const Search = (props) => {
+    return (
+        <div>
+            <div className='search'>
+                <h1>Search</h1>
+                <div className='search__form'>
+                    <Form inline>
+                        <FormControl type="text" placeholder="Search by movie title..." className="mr-sm-3" />
+                        <Button variant="outline-success">Search</Button>
+                    </Form>
+                </div>
+            </div>
+
+            <div className='card__wrapper'>
+            {props.movies.map((movie, index) => (
+                <MoviesList movies={movie} />
+            ))}
+            </div>
+        </div>
+    )
+}
+
+export default Search;
