284511d417e5c63a2eeb696706c2e13aa6c6d9b4
alexandrovbg <cecko.aleksandrov@gmail.com>
2021-06-04 16:41:51 +0300

    Update Search Component

--- client/src/components/Search/Search.js
+++ client/src/components/Search/Search.js
@@ -11,16 +11,16 @@ const Search = (props) => {
                 <h1>Search</h1>
                 <div className='search__form'>
                     <Form inline>
-                        <FormControl type="text" placeholder="Search by movie title..." className="mr-sm-3" />
-                        <Button variant="outline-success">Search</Button>
+                        <FormControl value={props.value} onChange={(event) => props.setSearchValue(event.target.value)} type="text" placeholder="Search by movie title..." className="mr-sm-3" />
+                        <Button type='submit' variant="outline-success">Search</Button>
                     </Form>
                 </div>
             </div>
 
             <div className='card__wrapper'>
-            {props.movies.map((movie, index) => (
-                <MoviesList movies={movie} />
-            ))}
+                {props.movies.map((movie, index) => (
+                    <MoviesList movies={movie} />
+                ))}
             </div>
         </div>
     )
