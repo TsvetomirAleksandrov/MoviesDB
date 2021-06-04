ef5228a61dd3c04522194446f6fcdf986c213314
alexandrovbg <cecko.aleksandrov@gmail.com>
2021-06-04 16:44:14 +0300

    Create routes file

---
+++ server/routes/routes.js
@@ -0,0 +1,12 @@
+const router = require('express').Router();
+
+// const mainController = require('./controllers/mainController');
+const authController = require('../controllers/authController');
+// const courseController = require('./controllers/courseController')
+// const isAuth = require('./middlewares/isAuth');
+
+// router.use('/', mainController);
+router.use('/auth', authController);
+// router.use('/course', isAuth, courseController)
+
+module.exports = router;
