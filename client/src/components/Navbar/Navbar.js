423814b707606c3c98086998f9bad2751c211f93
alexandrovbg <cecko.aleksandrov@gmail.com>
2021-06-04 16:42:03 +0300

    Update Navbar

--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -1,7 +1,15 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Redirect, useHistory } from "react-router-dom";
 import { Navbar, Nav, Button, Form, FormControl } from 'react-bootstrap';
 
-const Header = () => {
+const Header = (props) => {
+    let history = useHistory();
+
+    function handleSubmit(e) {
+        e.preventDefault();
+        history.push('/search');
+    }
+
     return (
         <div>
             <Navbar bg="light" expand="lg">
@@ -10,18 +18,17 @@ const Header = () => {
                 <Navbar.Collapse id="basic-navbar-nav">
                     <Nav className="mr-auto">
                         <Nav.Link href="/">Home</Nav.Link>
-                        <Nav.Link href="/login">Register</Nav.Link>
-                        <Nav.Link href="/register">Login</Nav.Link>
+                        <Nav.Link href="/login">Login</Nav.Link>
+                        <Nav.Link href="/register">Register</Nav.Link>
                     </Nav>
                     <Form inline>
-                        <FormControl type="text" placeholder="Search by movie title..." className="mr-sm-2" />
-                        <Button variant="outline-success">Search</Button>
-                        {/* this button shoud redirect to movie details of the movie user searched*/}
+                        <FormControl value={props.value} onChange={(event) => props.setSearchValue(event.target.value)} type="text" placeholder="Search by movie title..." className="mr-sm-2" />
+                        <Button onClick={handleSubmit} type='submit' variant="outline-success">Search</Button>
                     </Form>
                 </Navbar.Collapse>
             </Navbar>
         </div>
-    )
+    );
 }
 
-export default Header
+export default Header;
