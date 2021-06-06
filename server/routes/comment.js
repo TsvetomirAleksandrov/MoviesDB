4b5161919138cd446ff2fa4a918af5916ae7b75d
alexandrovbg <cecko.aleksandrov@gmail.com>
2021-06-06 18:30:36 +0300

    Create comment(notes) route

---
+++ server/routes/comment.js
@@ -0,0 +1,42 @@
+const express = require('express');
+const router = express.Router();
+const { Comment } = require("../models/Comment");
+const { auth } = require("../middlewares/authentication");
+
+//=================================
+//             Subscribe
+//=================================
+
+router.post("/saveComment", auth, (req, res) => {
+
+    const comment = new Comment(req.body)
+
+    if (req.body.content !== "" && req.body.content !== null) {
+        comment.save((err, comment) => {
+            console.log(err)
+            if (err) return res.json({ success: false, err })
+
+            Comment.find({ '_id': comment._id })
+                .populate('writer')
+                .exec((err, result) => {
+                    if (err) return res.json({ success: false, err })
+                    return res.status(200).json({ success: true, result })
+                })
+        })
+    } else {
+        console.log(`req body cannot be empty or null`);
+    }
+
+})
+
+router.post("/getComments", (req, res) => {
+
+    Comment.find({ "movieId": req.body.movieId })
+        .populate('writer')
+        .exec((err, comments) => {
+            if (err) return res.status(400).send(err)
+            res.status(200).json({ success: true, comments })
+        })
+});
+
+module.exports = router;
