dc8cb44c5a0abe393ff06778e572b53e7b2e5f82
alexandrovbg <cecko.aleksandrov@gmail.com>
2021-06-06 18:31:52 +0300

    Update Config

--- client/src/components/Config.js
+++ client/src/components/Config.js
@@ -1,2 +1,7 @@
   //SERVER ROUTES
 export const USER_SERVER = '/api/users';
+
+export const API_KEY = `5dcf7f28a88be0edc01bbbde06f024ab`;
+export const API_URL = `https://api.themoviedb.org/3/`;
+
+// https://api.themoviedb.org/3/search/movie?api_key=5dcf7f28a88be0edc01bbbde06f024ab&language=en-US&query=${searchValue}&page=1&include_adult=false
