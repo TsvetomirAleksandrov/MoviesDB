8451be6eafc3f432e337b5983a4c0fe8e8b88065
alexandrovbg <cecko.aleksandrov@gmail.com>
2021-06-06 18:31:59 +0300

    Update App Component

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -10,13 +10,14 @@ import Search from './Search/Search';
 import MovieDetails from './MovieDetails/MovieDetails';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
+import { API_URL, API_KEY } from './Config';
 
 function App() {
   const [movies, setMovies] = useState([]);
   const [searchValue, setSearchValue] = useState('');
 
   const getMovieRequest = async (searchValue) => {
-    const url = `https://api.themoviedb.org/3/search/movie?api_key=5dcf7f28a88be0edc01bbbde06f024ab&language=en-US&query=${searchValue}&page=1&include_adult=false`;
+    const url = `${API_URL}search/movie?api_key=${API_KEY}&language=en-US&query=${searchValue}&page=1&include_adult=false`;
 
     const response = await fetch(url);
     const responseJson = await response.json();
@@ -39,7 +40,7 @@ function App() {
           <Route exact path="/" component={Auth(HomePage, null)} />
           <Route exact path="/login" component={Auth(LoginPage, false)} />
           <Route exact path="/register" component={Auth(RegisterPage, false)} />
-          {/* <Route exact path="/movie/:movieTitle" component={Auth(MovieDetails, null)} /> */}
+          <Route exact path="/movie/:movieId" component={Auth(MovieDetails, null)} />
           <Route exact path="/search">
             <Search searchValue={searchValue} setSearchValue={setSearchValue} movies={movies} />
           </Route>
